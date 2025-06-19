c5ba3a22e2c4ae0d123f760cedcf6a0dd7aa36fe
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2025-06-20 00:02:55 +0530

    added codes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react';
+import { MenuIcon, SearchIcon, TicketPlus, User, XIcon } from 'lucide-react';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const {user}=useUser()
+
+  const {openSignIn}=useClerk()
+ const navigate = useNavigate();
+
+
+
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -21,7 +30,7 @@ function Navbar() {
         min-md:px-8 py-3 max-md:h-screen min-md:rounded-full 
         backdrop-blur bg-gradient-to-br from-black/80 via-zinc-900/70 to-black/80 
         md:bg-white/10 md:border border-gray-300/20 overflow-hidden 
-        transition-[width] duration-300 ${menuOpen ? 'max-md:w-full' : ''}`}>
+        transition-[width] duration-300 ${menuOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
 
         <XIcon 
           className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' 
@@ -37,15 +46,33 @@ function Navbar() {
 
       {/* Right Section */}
       <div className='flex items-center gap-8'>
+
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
-          Login
-        </button>
+       { !user ? (
+  
+    <button
+      onClick={openSignIn}
+      className='px-4 py-1 sm:px-7 sm:py-2 bg-secondary hover:bg-secondary-dull transition rounded-full font-medium cursor-pointer'
+    >
+      Login</button>):(
+
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Action label="My Bookings" labelIcon={
+              <TicketPlus width={15}/>} onClick={()=>navigate('/my-bookings')}/>
+            
+          </UserButton.MenuItems>
+        </UserButton>
+      )
+      
+      }
+  </div>
+       
         <MenuIcon 
           className='md:hidden w-8 h-8 cursor-pointer' 
           onClick={() => setMenuOpen(true)}
         />
-      </div>
+    
     </div>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Herosection from '../components/Herosection'
 
 function Home() {
   return (
-    <div>
+    <>
+    <Herosection/>
       
-    </div>
+    </>
   )
 }
 
